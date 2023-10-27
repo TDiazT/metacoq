00d999cf34b100c90f6ea4695155018a89a91477
Tomas <tomdtron@gmail.com>
2023-10-27 17:56:52 +0200

    [feat] Add most grefs and gapp

--- examples/quote_untyped.v
+++ examples/quote_untyped.v
@@ -1,6 +1,22 @@
 From MetaCoq.Template Require Import All.
+(* From Coq Require Import Strings.String. *)
+ (* Require Import String. *)
 
+(* Notation "'Def' x" := (ltac:(run_template_program (tmPrint x) (fun y => idtac))) (at level 10). *)
+
+(* Eval cbn in Def 2. *)
+(* Compute Def 4. *)
+
+Axiom (foo : nat).
 Goal True.
 Proof.
   quote_term 1 (fun y => pose y).
   quote_untyped_term x (fun x => pose x).
+
+  quote_untyped_term foo (fun x => pose x).
+  quote_untyped_term nat (fun x => pose x).
+  quote_untyped_term (foo nat) (fun x => pose x).
+  quote_untyped_term (foo nat 1 2) (fun x => pose x).
+  quote_untyped_term (foo nat (bool nat) 2) (fun x => pose x).
+  quote_untyped_term nil (fun x => pose x).
+  quote_untyped_term cons (fun x => pose x).
