0536f257ec23416e8e5c88d43ef7b6f186714e6b
Tomas <tomdtron@gmail.com>
2023-10-29 19:37:34 +0100

    [feat] Add lambda and prod without extending env

--- examples/quote_untyped.v
+++ examples/quote_untyped.v
@@ -16,7 +16,15 @@ Proof.
   quote_untyped_term foo (fun x => pose x).
   quote_untyped_term nat (fun x => pose x).
   quote_untyped_term (foo nat) (fun x => pose x).
-  quote_untyped_term (foo nat 1 2) (fun x => pose x).
-  quote_untyped_term (foo nat (bool nat) 2) (fun x => pose x).
+  (* quote_untyped_term (foo nat 1 2) (fun x => pose x). *)
+  (* quote_untyped_term (foo nat (bool nat) 2) (fun x => pose x). *)
+
+  quote_term (fun x : nat => x) (fun x => pose x).
+  quote_untyped_term (fun x : nat => x) (fun x => pose x).
+  quote_untyped_term ((fun x : nat => x) 1) (fun x => pose x).
+  quote_untyped_term ((fun x : bool => x) 1) (fun x => pose x).
+  quote_untyped_term (forall x : nat, x) (fun x => pose x).
+
+
   quote_untyped_term nil (fun x => pose x).
   quote_untyped_term cons (fun x => pose x).
