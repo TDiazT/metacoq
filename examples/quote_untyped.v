7cb95f64b8e3a18136cf3d6a7f822a553d87857b
Tomas <tomdtron@gmail.com>
2023-10-30 09:51:21 +0100

    [feat] First pass at sorts

--- examples/quote_untyped.v
+++ examples/quote_untyped.v
@@ -11,18 +11,31 @@ Axiom (foo : nat).
 Goal True.
 Proof.
   quote_term 1 (fun y => pose y).
-  quote_untyped_term x (fun x => pose x).
-
+  (* Var *)
   quote_untyped_term foo (fun x => pose x).
+
+  (* Ind *)
   quote_untyped_term nat (fun x => pose x).
+
+  (* App *)
   quote_untyped_term (foo nat) (fun x => pose x).
   (* quote_untyped_term (foo nat 1 2) (fun x => pose x). *)
   (* quote_untyped_term (foo nat (bool nat) 2) (fun x => pose x). *)
 
-  quote_term (fun x : nat => x) (fun x => pose x).
+  (* Lambda *)
   quote_untyped_term (fun x : nat => x) (fun x => pose x).
-  quote_untyped_term ((fun x : nat => x) 1) (fun x => pose x).
+
+  (* App with w.e. *)
   quote_untyped_term ((fun x : bool => x) 1) (fun x => pose x).
+
+  (* Sorts *)
+  quote_untyped_term Set (fun x => pose x).
+  quote_untyped_term Prop (fun x => pose x).
+  quote_untyped_term SProp (fun x => pose x).
+  quote_untyped_term Type (fun x => pose x).
+
+
+  (* Prod *)
   quote_untyped_term (forall x : nat, x) (fun x => pose x).
 
 
